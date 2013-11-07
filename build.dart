ce2d97d1e0d09c519a03f82316f9c48fb0ff187e
Herbert Poul <h.poul@tapo.at>
2013-11-07 09:19:44 +0100

    declared version 0.0.1

--- build.dart
+++ build.dart
@@ -3,8 +3,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:polymer/builder.dart';
+//import 'package:polymer/builder.dart';
 
 void main() {
-  build(entryPoints: ['web/example.html']);
+//  build(entryPoints: ['web/example.html']);
 }
